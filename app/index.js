1d2bb6ca6e0889610cd3d1b1e0bea8632e90ec6a
mirfanjazuli <mirfanjazuli7@gmail.com>
2023-06-16 10:04:36 +0700

    protect node_modules and .env

--- app/index.js
+++ app/index.js
@@ -11,19 +11,8 @@ const Article = require('./models/articleModel')
 
 app.use(express.json())
 
-// configure dotenv
-// dotenv.config()
-
 // connect to database
 require('./database/db')
-
-// const appendUrl = (url) => `${$VERSION_API}${url}`
-
-
-// import routes
-// const userRoute = require('./app/routes/userRoute');
-
-// app.use('/profile', userRoute)
 
 // routes
 app.get('/', (req, res) => {
@@ -37,17 +26,6 @@ const articleRoute = require('./routes/articleRoute')
 // routers
 app.use('/api/v1/users', userRoute)
 app.use('/api/v1/articles', articleRoute)
-
-// app.post('/signup', async(req, res) => {
-//   try {
-//     const user = await User.create(req.body)
-//     res.status(200).json(user)
-//   } catch (error) {
-//     console.log(error.message)
-//     res.status(500).json({message: error.message})
-//   }
-// })
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port http://localhost:${PORT}`)
